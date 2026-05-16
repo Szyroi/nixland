47604a9dba03d8ddb93f043c3203e52831ad25ab
Szyroi <117603175+Szyroi@users.noreply.github.com>
2026-05-17 00:45:09 +0200

    Changed Module & Flake

--- hl/default.nix
+++ hl/default.nix
@@ -16,6 +16,6 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    xdg.configFile."hypr".source = "${inputs.nixland}/hyprland/${cfg.profile}";
+    xdg.configFile."hypr".source = "${inputs.nixland}/hl/${cfg.profile}";
   };
 }
